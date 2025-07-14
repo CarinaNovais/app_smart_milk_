de4d75fbfd301ac35671d372155cad4dc4956804
CarinaNovais <carinapereiraN@outlook.com>
2025-07-14 16:32:45 -0300

    adicionando pagina depositosProdutor

---
+++ SmartMilk/lib/pages/depositosProdutor.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:app_smart_milk/components/navbar.dart';
+import 'package:app_smart_milk/components/menuDrawer.dart';
+
+const Color appBlue = Color(0xFF0097B2);
+
+class DepositosprodutorPage extends StatefulWidget {
+  const DepositosprodutorPage({super.key});
+
+  @override
+  State<DepositosprodutorPage> createState() => _DepositosprodutorPage();
+}
+
+class _DepositosprodutorPage extends State<DepositosprodutorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: appBlue,
+      appBar: const Navbar(
+        title: 'Depósitos',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      endDrawer: const MenuDrawer(),
+      body: const Center(
+        child: Text(
+          'Conteúdo da tela de depósitos será exibido aqui',
+          style: TextStyle(color: Colors.white, fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
